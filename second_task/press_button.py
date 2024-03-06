a463ff462af898d97ce92de1ba1cc33e4262778a
podpivasik <114077096+podpivasik@users.noreply.github.com>
2024-03-06 21:26:53 +0300

    task 2.1

--- second_task/press_button.py
+++ second_task/press_button.py
@@ -1,5 +1,4 @@
-from PySide6.QtCore import Qt
-from PySide6.QtGui import QFont, QPixmap
+from PySide6.QtGui import QFont
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
 
 
@@ -8,7 +7,44 @@ class Window(QMainWindow):
         super(Window, self).__init__()
         self.setWindowTitle("You won't be able to click on me.")
 
-        self.label = QLabel("Отпущена", self)
-        self.QFont("Times", 20)
+        self.label = QLabel("Кнопка Отпущена", self)
+        self.label.setFixedSize(300, 50)
+        font = QFont("Times", 20)
+        font.setBold(True)
+        self.label.setFont(font)
+        self.label.setGeometry(190, 100, self.width(), self.height())
 
         self.btn = QPushButton(self)
+        self.btn.setCheckable(True)
+        self.btn.setText("Попробуй нажать")
+        self.btn.move(215, 400)
+        self.btn.setFixedSize(200, 50)
+        self.btn.pressed.connect(self.onButtonPressed)
+        self.btn.released.connect(self.onButtonReleased)
+        self.btn.setStyleSheet(
+            "QPushButton{"
+            "background-color: lightblue;"
+            "border: 2px solid;"
+            "border-radius: 7px;"
+            "}"
+            "QPushButton:hover{"
+            "background-color: lightgreen;"
+            "}"
+            "QPushButton:pressed{"
+            "background-color: lightcoral;"
+            "}"
+        )
+
+    def onButtonPressed(self):
+        self.label.setText("Кнопка нажата")
+
+    def onButtonReleased(self):
+        self.label.setText("Кнопка отпущена")
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Window()
+    window.setGeometry(430, 200, 600, 600)
+    window.show()
+    app.exec()
